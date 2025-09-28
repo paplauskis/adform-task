daeb8831cf21e2ebb96b617c296ef7fec19e4539
paplauskis <paplauskaskostas@gmail.com>
2025-09-28 19:27:26 +0300

    create query that gets order items and total price of order

--- sql/queries.sql
+++ sql/queries.sql
@@ -19,6 +19,34 @@ FROM order_item oi
 JOIN product p ON p.id = oi.product_id
 WHERE oi.order_id = $1;
 
+-- parameterized query that gets order items and total price of order in the same table
+PREPARE get_order_invoice(int) AS
+(
+    SELECT 
+        p.name AS product_name,
+        c.category_name,
+        oi.quantity AS product_quantity,
+        p.price AS single_product_price,
+        (oi.quantity * p.price) AS line_total
+    FROM order_item oi
+    JOIN product p ON p.id = oi.product_id
+    JOIN category c ON c.id = p.category_id
+    WHERE oi.order_id = $1
+)
+UNION ALL
+(
+    SELECT 
+        'ORDER TOTAL' AS product_name,
+        NULL AS category_name,
+        NULL AS product_quantity,
+        NULL AS single_product_price,
+        SUM(oi.quantity * p.price) AS line_total
+    FROM order_item oi
+    JOIN product p ON p.id = oi.product_id
+    WHERE oi.order_id = $1
+);
+
 -- queries can be executed like this
 EXECUTE get_order_items(10);
 EXECUTE get_order_total_amount(10);
+EXECUTE get_order_invoice(10);

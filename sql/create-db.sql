2b9f959349bf056164a22599c01f099d5b97eaa4
paplauskis <paplauskaskostas@gmail.com>
2025-09-29 08:12:57 +0300

    Delete sql directory

--- sql/create-db.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE "adform-task" WITH ENCODING = 'UTF8'
--- sql/functions.sql
+++
@@ -1,8 +0,0 @@
--- function that generates random ints from 1 to 100
-CREATE OR REPLACE FUNCTION random_products_count(order_id INTEGER DEFAULT 1) 
-RETURNS INTEGER AS $$ 
-BEGIN
-    PERFORM setseed((order_id * random())::float8 / 2147483647);
-    RETURN (1 + FLOOR(random() * 100))::int; 
-END; 
-$$ LANGUAGE plpgsql VOLATILE;
